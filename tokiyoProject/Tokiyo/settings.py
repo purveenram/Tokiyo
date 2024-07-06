176777ab8bcf7ecc64830192fa3406d23b7a8829
purveenram <purveenram.36@gmail.com>
2024-07-06 14:57:42 +0530

    new change for db tmp

--- tokiyoProject/Tokiyo/settings.py
+++ tokiyoProject/Tokiyo/settings.py
@@ -67,16 +67,9 @@ WSGI_APPLICATION = 'Tokiyo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Keep the development database location
+        'NAME':'../tmp/db.sqlite3',  # Keep the development database location
     }
 }
-
-# Production settings for Vercel
-if not DEBUG:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',  # Use /tmp directory for production database
-    }
 
 # for credential check
 AUTHENTICATION_BACKENDS = [
