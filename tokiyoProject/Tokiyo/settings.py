fb2fc91c4373143f3feec8ae1f8b2f5f62f81a9c
purveenram <purveenram.36@gmail.com>
2024-07-08 14:27:49 +0530

    changes

--- tokiyoProject/Tokiyo/settings.py
+++ tokiyoProject/Tokiyo/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'Tokiyo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':  '/tmp/db.sqlite3',
     }
 }
 
