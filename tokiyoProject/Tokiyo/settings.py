092569fa311869ec186a6247700363ff133124be
purveenram <purveenram.36@gmail.com>
2024-07-08 13:48:20 +0530

    db chnages

--- tokiyoProject/Tokiyo/settings.py
+++ tokiyoProject/Tokiyo/settings.py
@@ -67,9 +67,17 @@ WSGI_APPLICATION = 'Tokiyo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
     }
 }
+
+# Production settings for Vercel
+if not DEBUG:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
+    }
+
 
 # for credential check
 AUTHENTICATION_BACKENDS = [
